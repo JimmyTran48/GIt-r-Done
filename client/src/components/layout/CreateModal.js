c03782fea739323da06671a52ab53aa3750e7d98
Jimpulse48 <107091480+Jimpulse48@users.noreply.github.com>
2023-05-10 21:24:46 -0500

    refactored createmodal

--- client/src/components/layout/CreateModal.js
+++ client/src/components/layout/CreateModal.js
@@ -4,12 +4,10 @@ import React from 'react';
 import CreateForm from '../forms/CreateForm';
 //  importing utils
 import Modal from '../UI/Modal';
-//  importing styles
-import classes from './CreateModal.module.scss';
 
 const CreateModal = (props) => {
   return (
-    <Modal onClose={props.onClose} className={classes.modal}>
+    <Modal onClose={props.onClose}>
       <CreateForm
         onClose={props.onClose}
         users={props.users}
